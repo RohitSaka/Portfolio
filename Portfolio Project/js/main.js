8faa85b0983a38598700ff81554c57a40ebf5282
Rohit <62130251+RohitSaka@users.noreply.github.com>
2020-05-15 06:40:24 +0530

    My Portfolio Project

---
+++ Portfolio Project/js/main.js
@@ -0,0 +1,22 @@
+// Sticky Navigation Menu //
+$(document).ready(function () {
+    // sticky navigation menu
+
+    let nav_offset_top = $('.header_area').height();
+
+    function navbarFixed() {
+        if ($('.header_area').length) {
+            $(window).scroll(function () {
+                let scroll = $(window).scrollTop();
+                if (scroll >= nav_offset_top) {
+                    $('.header_area .main-menu').addClass('navbar_fixed');
+                } else {
+                    $('.header_area .main-menu').removeClass('navbar_fixed');
+                }
+            })
+        }
+    }
+
+    navbarFixed();
+
+});
